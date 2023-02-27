c67a2285d24ff81518775e0cf08e93ba3c686ec3
Kate Bolshakova <katassr@mail.ru>
2023-02-27 17:37:35 +0300

    feat: add task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        this.candidat = Math.round((this.min + this.max) / 2);
+        return this.candidat;
     }
 
     lower() {
-
+        this.max = this.candidat;
+        return this.max;
     }
 
-    greater() {
 
+    greater() {
+        this.min = this.candidat;
+        return this.min;
     }
 }
 
